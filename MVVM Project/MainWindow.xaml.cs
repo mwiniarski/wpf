6c3210bba084e06555af0acb35fcae405bb41852
Michal <Michal>
2017-11-06 20:29:19 +0100

    init

---
+++ MVVM Project/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.FontSize = e.NewSize.Height*0.035;
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListBox listbox = sender as ListBox;
+            ListBoxItem itm = new ListBoxItem();
+            itm.Content = "KUPA123";
+            listbox.Items.Add(itm);
+
+            ItemCollection a = listbox.Items;
+            foreach(ListBoxItem f in a) 
+            {
+                f.Content += "Z";
+            }
+        }
+    }
+}
